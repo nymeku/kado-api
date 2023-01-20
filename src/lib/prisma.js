c126b53ec1ab85f76569f121e1761e37caeef5c0
nymeku <arnaud.andrieu@epitech.eu>
2023-01-20 12:02:21 +0100

    clean repo

--- src/lib/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = global.prisma ||
-    new client_1.PrismaClient({
-        log: ["error"],
-    });
-exports.default = prisma;
